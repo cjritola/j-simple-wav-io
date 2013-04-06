879f257d2639d493ebd5cc5e992af88eadbc3b96
cjritola <cjritola@b7ac3506-8c68-bf7e-baa5-724ac1d93e3d>
2013-04-06 16:00:37 +0000

    Updated copyright

--- SimpleWAVIO/src/com/ritolaaudio/simplewavio/files/riff/wave/RiffChunk_data.java
+++ SimpleWAVIO/src/com/ritolaaudio/simplewavio/files/riff/wave/RiffChunk_data.java
@@ -1,21 +1,21 @@
+/*******************************************************************************
+ * SimpleWAVIO - A straightforward library for loading and saving .WAV (RIFF) files as numerical arrays.
+ * Copyright (C) 2012,2013  Chuck Ritola
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package com.ritolaaudio.simplewavio.files.riff.wave;
-/**
-SimpleWAVIO - A straightforward library for loading and saving .WAV (RIFF) files as numerical arrays.
-Copyright (C) 2012  Chuck Ritola
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 
